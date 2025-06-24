2439e5580fc95c3abc485f479dd934daeba00742
Valentin Kiryanski <94051649+valentinkiryanski@users.noreply.github.com>
2025-06-24 13:37:58 +0300

    Fixed an issue with authenticating the user with role "ADMIN"

--- src/main/java/com/ecommercenext/nextecommerce/security/DemoSecurityConfig.java
+++ src/main/java/com/ecommercenext/nextecommerce/security/DemoSecurityConfig.java
@@ -24,7 +24,7 @@ public class DemoSecurityConfig {
         return userManager;
     }
 
-     @Bean
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
